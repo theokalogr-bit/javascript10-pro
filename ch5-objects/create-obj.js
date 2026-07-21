03b4ab46d8e8320444b8147a0278356654c334f9
theokalogr <theokalogr@gmail.com>
2026-07-21 21:01:04 +0300

    feat: add initial object creation examples including empty object, point, and person

---
+++ ch5-objects/create-obj.js
@@ -0,0 +1,13 @@
+const obj = {}      // empty object
+const point = { x: 1, y: 2 }
+point.z = 4
+
+const person = {
+    firstname: "Alice",
+    lastname: "W.",
+    hobbies: [tennis, football, piano],
+    home: { x: 234567.65, y: 334456.99 },
+    geometry: [{x: 23445, y: 1234.76}, {x: 123333.55, y: 34567}, {x: 1299333.55, y: 30067}],
+    active: true,
+    age: 45
+}
